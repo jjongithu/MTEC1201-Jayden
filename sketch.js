a5869d673bb435a1fde36a1b9a2ee7c0b0502d62
jjongithu <Jayden.Galarza28@login.cuny.edu>
2026-09-04 12:25:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,14 @@ function setup() {
   createCanvas(400, 400);
 }
 
-function draw() {
-  background(220);
-}
+function draw () {
+  background "blue"; 
+}  
+
+function draw () {}
+background "blue";
+
+rect{0, 0, 200, 200}
+
+
+
